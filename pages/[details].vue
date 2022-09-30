a6a1f7c117b066d43d33df3e350cdaa8bf173ed6
tenderking <gmu003@gmail.com>
2022-10-01 00:36:22 +0200

    style: added styles

--- pages/[details].vue
+++ pages/[details].vue
@@ -1,3 +1,33 @@
+<script setup>
+	const route = useRoute();
+	const countryDetails = ref(null);
+	const onError = ref(null);
+	const onPending = ref(null);
+	const url = ref(
+		"https://restcountries.com/v3.1/name/" + route.params.details
+	);
+	async function initData() {
+		const { data: response, pending, error } = await useFetch(url);
+		countryDetails.value = response.value;
+		onPending.value = pending.value;
+		onError.value = error.value;
+	}
+	watch(
+		() => route.params.details,
+		() => {
+			console.log("params Details changed");
+			initData();
+		}
+	);
+	// initData();
+	onMounted(() => {
+		console.log("yippi");
+		initData();
+	});
+	onUnmounted(() => {
+		console.log("kaye");
+	});
+</script>
 <template>
 	<div class="details-page">
 		<NuxtLink to="/">
@@ -8,95 +38,115 @@
 						height="18"
 						viewBox="0 0 18 18"
 						fill="none"
+						class="arrow"
 						xmlns="http://www.w3.org/2000/svg"
+						stroke="var(--fill-color)"
 					>
 						<path
 							fill-rule="evenodd"
 							clip-rule="evenodd"
 							d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
-							fill="white"
+							fill="var(--fill-color)"
 						/>
 					</svg>
 				</i>
-				back
+				<p>back</p>
 			</button>
 		</NuxtLink>
-		<div class="details-container">
-			<div v-for="country in countryDetails" :key="country.name.common">
-				<img :src="country.flags.png" alt="flag" />
-				<div class="basic-details">
-					<h2>Details {{ route.params.details }}</h2>
-					<h3>
-						Native name:
-						<p v-for="native in country.name.nativeName">
-							{{ native.official }}, &nbsp;
-						</p>
-					</h3>
+		<div v-if="onPending">Loading...</div>
+		<div v-if="onError">Something went wrong...</div>
+		<div v-else>
+			<div v-show="countryDetails" class="details-container">
+				<template v-for="country in countryDetails" :key="country.name.common">
+					<img :src="country.flags.png" alt="flag" />
 
-					<h3>
-						Population:
-						<p>{{ country.population }}</p>
-					</h3>
+					<div class="basic-details">
+						<h2>Details {{ route.params.details }}</h2>
 
-					<h3>
-						Region:
-						<p>{{ country.region }}</p>
-					</h3>
+						<h3>
+							Native name:
+							<p v-for="native in country.name.nativeName">
+								{{ native.official }}, &nbsp;
+							</p>
+						</h3>
 
-					<h3>
-						Sub Region:
-						<p>{{ country.region }}</p>
-					</h3>
+						<h3>
+							Population:
+							<p>{{ country.population }}</p>
+						</h3>
 
-					<h3>
-						Capital:
-						<p v-for="lan in country.capital">{{ lan }}, &nbsp;</p>
-					</h3>
-				</div>
+						<h3>
+							Region:
+							<p>{{ country.region }}</p>
+						</h3>
+
+						<h3>
+							Sub Region:
+							<p>{{ country.region }}</p>
+						</h3>
+
+						<h3>
+							Capital:
+							<p v-for="lan in country.capital">{{ lan }}, &nbsp;</p>
+						</h3>
+					</div>
+				</template>
+
+				<template v-for="item in countryDetails" :key="item.name.common">
+					<div class="special-details">
+						<h3>
+							Top level Domain:
+							<p>{{ item.tld[0] }}</p>
+						</h3>
+
+						<h3>
+							Currencies:
+							<p v-for="currency in item.currencies">{{ currency.name }}</p>
+						</h3>
+
+						<h3>
+							Langauges:
+							<p v-for="lan in item.languages">{{ lan }}, &nbsp;</p>
+						</h3>
+					</div>
+
+					<div class="border">
+						<h3 v-show="item.borders">Border countries</h3>
+						<span v-for="neighbour in item.borders"
+							>{{ neighbour }}, &nbsp;</span
+						>
+					</div>
+				</template>
 			</div>
-			<template v-for="item in countryDetails" :key="item.name.common">
-				<div class="special-details">
-					<h3>
-						Top level Domain:
-						<p>{{ item.tld[0] }}</p>
-					</h3>
-
-					<h3>
-						Currencies:
-						<p v-for="currency in item.currencies">{{ currency.name }}</p>
-					</h3>
-
-					<h3>
-						Langauges:
-						<p v-for="lan in item.languages">{{ lan }}, &nbsp;</p>
-					</h3>
-				</div>
-
-				<div class="border">
-					<h3>Border countries</h3>
-					<span v-for="neighbour in item.borders">{{ neighbour }}, &nbsp;</span>
-				</div>
-			</template>
 		</div>
-		<!-- <pre> {{ countryDetails }}</pre> -->
 	</div>
 </template>
-<script setup>
-	const route = useRoute();
-	const url = "https://restcountries.com/v3.1/name/" + route.params.details;
-	const { data: countryDetails } = await useFetch(url);
-</script>
+
 <style lang="sass" scoped>
 	.details-page
 		padding: 2rem
-		button
-			padding: 0.5rem 1rem
-			box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px
-			display: flex
-			justify-content: center
-			gap: 0.5rem
+		height: 100%
+		a
+			text-decoration: none
+			color:inherit
+			button
+				padding: 0.5rem 1rem
+				display: flex
+				box-shadow: var(--shadow)
+				border: none
+				background-color: var(--elements)
+				justify-content: center
+				gap: 0.5rem
+				margin-bottom: 1rem
+				p
+					margin:0
+
+
+				.arrow
+					stroke:green
+
 		img
-			border-radius: 1rem
+			border-radius: var(--radius)
 			margin-block: 2rem
 		h2
 			margin-bottom: 2rem
@@ -106,18 +156,40 @@
 				font-weight: normal
 
 	@media (min-width:650px)
-		.details-container
-			display: grid
-			grid-template-columns: repeat(4,1fr)
-			grid-template-rows: repeat(4,1fr)
-			img
-				grid-column:  1/3
-				grid-row: 1/3
-			.basic-details
-				grid-column:  3/4
-				grid-row: 1/span3
-			.special-details
-				grid: 4/5 2/span2
-			.border
-				grid-area: 3/span2 4/5
+		.details-page
+			button
+				box-shadow: initial
+			div
+				height: 100%
+				padding: 1em
+
+
+				.details-container
+					display: grid
+					grid-template-columns: repeat(4,1fr)
+					grid-template-rows: repeat(4,1fr)
+					// align-items: center
+					gap:1rem
+
+
+					img
+						grid-column:  1/3
+						grid-row: 1/4
+						// justify-self: center
+						width: 100%
+					.basic-details
+						grid-column:  3/4
+						grid-row: 1/span2
+						grid-template-columns: subgrid
+						h2
+							grid-row: 1/2
+					.special-details
+						padding-top:4rem
+						grid-column: 4/5
+						grid-row:  1/3
+						justify-self: center
+						align-self: start
+					.border
+						grid-column: 3/span2
+						grid-row:  3/4
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,3 @@
-<template>
-	<main>
-		<header class="query">
-			<div class="search-input">
-				<svg
-					width="16"
-					height="16"
-					viewBox="0 0 16 16"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						fill-rule="evenodd"
-						clip-rule="evenodd"
-						d="M11.1111 9.77778H10.4L10.1333 9.51111C11.0222 8.53333 11.5556 7.2 11.5556 5.77778C11.5556 2.57778 8.97778 0 5.77778 0C2.57778 0 0 2.57778 0 5.77778C0 8.97778 2.57778 11.5556 5.77778 11.5556C7.2 11.5556 8.53333 11.0222 9.51111 10.1333L9.77778 10.4V11.1111L14.2222 15.5556L15.5556 14.2222L11.1111 9.77778ZM5.77778 9.77778C3.55556 9.77778 1.77778 8 1.77778 5.77778C1.77778 3.55556 3.55556 1.77778 5.77778 1.77778C8 1.77778 9.77778 3.55556 9.77778 5.77778C9.77778 8 8 9.77778 5.77778 9.77778Z"
-						fill="#B2B2B2"
-					/>
-				</svg>
-
-				<input
-					type="search"
-					placeholder="search for a country"
-					@keyup.enter="getCountry($event)"
-					v-model.lazy="searchQuery"
-				/>
-			</div>
-			<select
-				name="Filter By Region"
-				id="filter-drop-down"
-				v-model="regionSelected"
-			>
-				<option disabled selected hidden value="">Filter by region</option>
-				<option v-for="region in Regions" :key="region">
-					{{ region }}
-				</option>
-			</select>
-		</header>
-		<main class="main">
-			<div class="card" v-for="country in allCountries">
-				<NuxtLink :to="country.name.common">
-					<img :src="country.flags.png" :alt="country.name.common" />
-					<div class="card-text">
-						<h2>
-							{{ country.name.common }}
-						</h2>
-						<div>
-							<span>
-								<h3>Population:</h3>
-								<p>
-									{{ country.population }}
-								</p>
-							</span>
-						</div>
-						<div>
-							<h3>Region:</h3>
-							<p>
-								{{ country.region }}
-							</p>
-						</div>
-						<div>
-							<h3>Capital:</h3>
-							<p v-for="capital in country.capital" :key="capital">
-								<span>
-									{{ capital }}
-								</span>
-							</p>
-						</div>
-					</div>
-				</NuxtLink>
-			</div>
-		</main>
-	</main>
-</template>
 <script setup>
 	/* Variables  */
 
@@ -119,66 +46,150 @@
 			"https://restcountries.com/v3.1/name/" + searchQuery.value;
 	});
 </script>
+<template>
+	<div class="main">
+		<header class="query-nav">
+			<div class="search-input">
+				<svg
+					width="16"
+					height="16"
+					viewBox="0 0 16 16"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M11.1111 9.77778H10.4L10.1333 9.51111C11.0222 8.53333 11.5556 7.2 11.5556 5.77778C11.5556 2.57778 8.97778 0 5.77778 0C2.57778 0 0 2.57778 0 5.77778C0 8.97778 2.57778 11.5556 5.77778 11.5556C7.2 11.5556 8.53333 11.0222 9.51111 10.1333L9.77778 10.4V11.1111L14.2222 15.5556L15.5556 14.2222L11.1111 9.77778ZM5.77778 9.77778C3.55556 9.77778 1.77778 8 1.77778 5.77778C1.77778 3.55556 3.55556 1.77778 5.77778 1.77778C8 1.77778 9.77778 3.55556 9.77778 5.77778C9.77778 8 8 9.77778 5.77778 9.77778Z"
+						fill="#B2B2B2"
+					/>
+				</svg>
+				<input
+					type="search"
+					placeholder="search for a country"
+					@keyup.enter="getCountry($event)"
+					v-model.lazy="searchQuery"
+				/>
+			</div>
+
+			<select
+				name="Filter By Region"
+				id="filter-drop-down"
+				v-model="regionSelected"
+			>
+				<option disabled selected hidden value="">Filter by region</option>
+				<option v-for="region in Regions" :key="region">
+					{{ region }}
+				</option>
+			</select>
+		</header>
+		<main class="card-container">
+			<div class="card" v-for="country in allCountries">
+				<NuxtLink :to="country.name.common" :replace="true">
+					<img :src="country.flags.png" :alt="country.name.common" />
+					<div class="card-text">
+						<h2>
+							{{ country.name.common }}
+						</h2>
+						<div>
+							<span>
+								<h3>Population:</h3>
+								<p>
+									{{ country.population }}
+								</p>
+							</span>
+						</div>
+						<div>
+							<h3>Region:</h3>
+							<p>
+								{{ country.region }}
+							</p>
+						</div>
+						<div>
+							<h3>Capital:</h3>
+							<p v-for="capital in country.capital" :key="capital">
+								<span>
+									{{ capital }}
+								</span>
+							</p>
+						</div>
+					</div>
+				</NuxtLink>
+			</div>
+		</main>
+	</div>
+</template>
+
 <style lang="sass" scoped>
-		.query
-			display: flex
-			flex-direction: column
-			gap: 1rem
-
-			select
-				width: fit-content
-				padding: 0.5rem 2rem
-				border-radius: 10px
-				outline: none
-				border:0
-				box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px
-				option
-					top:2rem
-
-			.search-input
-				padding: 0.5rem 2rem
-				border-radius: 10px
-				box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px
+			.query-nav
 				display: flex
-				gap: 1rem
+				flex-direction: column
+				gap: 2rem
+				margin-top:2rem
+				padding-inline: 1rem
+				select
+					width: fit-content
+					padding: 0.5rem 2rem
+					border-radius: var(--radius)
+					outline: none
+					border:0
+					box-shadow: var(--shadow)
+					background-color: var(--elements)
+					option
+						top:2rem
+
+				.search-input
+					padding: 0.5rem 0.25rem 0.5rem 1rem
+					border-radius: var(--radius)
+					box-shadow: var(--shadow)
+					display: flex
+					gap: 1rem
+					align-items: center
+					color: var(--input)
+					background-color: var(--elements)
+					input
+						border: none
+
+			.card-container
+				display: flex
+				flex-direction: column
+				gap: 2rem
 				align-items: center
-				input
-					border: none
+				padding-block: 2rem
 
-		.main
-			display: flex
-			flex-direction: column
-			gap: 2rem
-			align-items: center
-			padding-block: 2rem
 
-			.card
-				border-radius: 1rem
-				box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px
-				overflow: hidden
-				a
-					text-decoration: none
-					color: inherit
+				.card
+					border-radius: var(--radius)
+					box-shadow: var(--shadow)
+					overflow: hidden
+					background: var(--elements)
+					a
+						text-decoration: none
+						color: inherit
+						img
+							min-height: max-content
 
-					.card-text
-						padding: 2rem
+						.card-text
+							padding: 2rem
+							max-width: 30ch
 
-						h2
-							padding-bottom: 2rem
 
-						p, h3
-							display: inline-block
-							padding-bottom: 1rem
+							h2
+								padding-bottom: 2rem
+
+							p, h3
+								display: inline-block
+								padding-bottom: 1rem
 
 
 	@media (min-width:650px)
-	.query
-			flex-direction: row
-			justify-content: space-between
-			padding: 2rem
-	.main
-		display: grid
-		grid-template-columns: repeat(auto-fit,20rem)
-		justify-content: center
-		padding:2rem
+		.query-nav
+				flex-direction: row
+				justify-content: space-between
+				padding: 2rem
+		.card-container
+			display: grid
+			grid-template-columns: repeat(auto-fit,20rem)
+			justify-content: center
+			padding:2rem
 </style>
