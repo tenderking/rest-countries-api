70764446bb1605ec4ddd1beb2976d4258c6b80f1
George <gmu003@gmail.com>
2022-10-03 18:58:15 +0200

    fix: async bug added darkmode

--- app.vue
+++ app.vue
@@ -1,96 +1,111 @@
 <script setup>
-	useHead({
-		link: [
-			{
-				rel: "preconnect",
-				href: "https://fonts.googleapis.com",
-			},
-			{
-				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap",
-				crossorigin: "",
-			},
-		],
-	});
+useHead({
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap",
+      crossorigin: "",
+    },
+  ],
+});
+const isDark = useDark({
+  selector: "#__nuxt",
+  valueDark: "dark",
+  valueLight: "light",
+});
+
+const toggleDark = useToggle(isDark);
 </script>
 <template>
-	<nav>
-		<h1>Where in the world?</h1>
-		<button>
-			<i> 🌙</i>
-			<h2>Dark Mode</h2>
-		</button>
-	</nav>
-	<NuxtPage :page-key="$route.fullPath" />
+  <nav>
+    <h1>Where in the world?</h1>
+    <button @click.prevent="toggleDark()">
+      <i> 🌙</i>
+      <h2>Dark Mode</h2>
+    </button>
+  </nav>
+  <NuxtPage :page-key="$route.fullPath" />
 </template>
 <style>
-	* {
-		border-block: box;
-		margin: 0;
-	}
+* {
+  border-block: box;
+  margin: 0;
+}
 
-	:root {
-		--lm-bg: hsl(0, 0%, 98%);
-		--lm-color: hsl(200, 15%, 8%);
-		--dm-bg: hsl(207, 26%, 17%);
-		--dm-color: hsl(0, 0%, 100%);
-		--lm-elements: hsl(0, 0%, 100%);
-		--dm-elements: hsl(209, 23%, 22%);
-		--lm-input: hsl(0, 0%, 52%);
-		--shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
-			rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
-		--radius: 10px;
-		--fill-color: hsl(200, 15%, 8%);
-	}
-	input,
-	button {
-		text-decoration: none;
-		font: inherit;
-	}
-	.light {
-		background-color: var(--lm-bg);
-		color: var(--lm-color);
-		--elements: var(--lm-elements);
-		--input: var(--lm-input);
-		--fill-color: var(--lm-color);
-	}
-	.dark {
-		background-color: var(--dm-bg);
-		color: var(--dm-color);
-		--elements: var(var(--dm-elements));
-		--input: var(--dm-elements);
-		--fill-color: var(--dm-color);
-	}
-	#__nuxt {
-		background-color: var(--lm-bg);
-		color: var(--lm-color);
-		--elements: var(--lm-elements);
-		--input: var(--lm-input);
-		font-family: "Nunito Sans", sans-serif;
-		font-size: 14px;
-		height: 100vh;
-	}
+:root {
+  --lm-bg: hsl(0, 0%, 98%);
+  --lm-color: hsl(200, 15%, 8%);
+  --dm-bg: hsl(207, 26%, 17%);
+  --dm-color: hsl(0, 0%, 100%);
+  --lm-elements: hsl(0, 0%, 100%);
+  --dm-elements: hsl(209, 23%, 22%);
+  --lm-input: hsl(0, 0%, 52%);
+  --shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+    rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+  --radius: 10px;
+  --fill-color: hsl(200, 15%, 8%);
+  --bg-color: var(--lm-bg);
+  /* --color: var(--lm-color); */
+  --elements: var(--lm-elements);
+  --input: var(--lm-input);
+}
+
+.light {
+  --bg-color: var(--lm-bg);
+  --color: var(--lm-color);
+  --elements: var(--lm-elements);
+  --input: var(--lm-input);
+  --fill-color: var(--lm-color);
+  --input: var(--lm-input);
+}
+.dark {
+  --bg-color: var(--dm-bg);
+  --color: var(--dm-color);
+  --elements: var(--dm-elements);
+  --input: var(--dm-color);
+  --fill-color: var(--dm-color);
+}
+#__nuxt {
+  background-color: var(--bg-color);
+  color: var(--color);
+
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 14px;
+  height: 100vh;
+}
+input,
+button {
+  text-decoration: none;
+  font: inherit;
+}
 </style>
 <style lang="sass">
 
-	nav
+nav
+	display: flex
+	justify-content: space-between
+	padding: 1rem
+	background: var(--elements)
+	box-shadow: var(--shadow)
+	h1
+		font-size: 24px
+		color: var(--color)
+	button
 		display: flex
-		justify-content: space-between
-		padding: 1rem
-		background: var(--elements)
-		box-shadow: var(--shadow)
-		h1
-			font-size:24px
-		button
-			display: flex
-			align-items: center
-			background: 0
-			border: none
-			gap:5px
-			h2
-				font-weight:600
-				font-size:16px
-	@media (min-width:650px)
-		nav
-			padding-inline: 2rem
+		align-items: center
+		background: 0
+		border: none
+		gap: 5px
+		h2
+			font-weight: 600
+			font-size: 16px
+			color: var(--color)
+
+@media (min-width:650px)
+	nav
+		padding-inline: 2rem
 </style>
--- pages/[details].vue
+++ pages/[details].vue
@@ -1,195 +1,177 @@
 <script setup>
-	const route = useRoute();
-	const countryDetails = ref(null);
-	const onError = ref(null);
-	const onPending = ref(null);
-	const url = ref(
-		"https://restcountries.com/v3.1/name/" + route.params.details
-	);
-	async function initData() {
-		const { data: response, pending, error } = await useFetch(url);
-		countryDetails.value = response.value;
-		onPending.value = pending.value;
-		onError.value = error.value;
-	}
-	watch(
-		() => route.params.details,
-		() => {
-			console.log("params Details changed");
-			initData();
-		}
-	);
-	// initData();
-	onMounted(() => {
-		console.log("yippi");
-		initData();
-	});
-	onUnmounted(() => {
-		console.log("kaye");
-	});
+const route = useRoute();
+
+const url = ref("https://restcountries.com/v3.1/name/" + route.params.details);
+const countryDetails = ref(null);
+const { pending, data, error } = await useFetch(url, { initialCache: false });
+countryDetails.value = await data.value;
+console.log("response: ", countryDetails.value);
+
+watch(
+  () => route.params.details,
+  () => {
+    console.log("params Details changed");
+  }
+);
 </script>
 <template>
-	<div class="details-page">
-		<NuxtLink to="/">
-			<button>
-				<i
-					><svg
-						width="18"
-						height="18"
-						viewBox="0 0 18 18"
-						fill="none"
-						class="arrow"
-						xmlns="http://www.w3.org/2000/svg"
-						stroke="var(--fill-color)"
-					>
-						<path
-							fill-rule="evenodd"
-							clip-rule="evenodd"
-							d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
-							fill="var(--fill-color)"
-						/>
-					</svg>
-				</i>
-				<p>back</p>
-			</button>
-		</NuxtLink>
-		<div v-if="onPending">Loading...</div>
-		<div v-if="onError">Something went wrong...</div>
-		<div v-else>
-			<div v-show="countryDetails" class="details-container">
-				<template v-for="country in countryDetails" :key="country.name.common">
-					<img :src="country.flags.png" alt="flag" />
+  <div class="details-page">
+    <NuxtLink to="/">
+      <button>
+        <i
+          ><svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            class="arrow"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="var(--fill-color)"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
+              fill="var(--fill-color)"
+            />
+          </svg>
+        </i>
+        <p>back</p>
+      </button>
+    </NuxtLink>
+    <div v-if="pending">Loading...</div>
+    <div v-if="error">Something went wrong...</div>
+    <div v-else>
+      <div v-show="countryDetails" class="details-container">
+        <template v-for="country in countryDetails" :key="country.name.common">
+          <img :src="country.flags.png" alt="flag" />
 
-					<div class="basic-details">
-						<h2>Details {{ route.params.details }}</h2>
+          <div class="basic-details">
+            <h2>Details {{ route.params.details }}</h2>
 
-						<h3>
-							Native name:
-							<p v-for="native in country.name.nativeName">
-								{{ native.official }}, &nbsp;
-							</p>
-						</h3>
+            <h3>
+              Native name:
+              <p v-for="native in country.name.nativeName">
+                {{ native.official }}, &nbsp;
+              </p>
+            </h3>
 
-						<h3>
-							Population:
-							<p>{{ country.population }}</p>
-						</h3>
+            <h3>
+              Population:
+              <p>{{ country.population }}</p>
+            </h3>
 
-						<h3>
-							Region:
-							<p>{{ country.region }}</p>
-						</h3>
+            <h3>
+              Region:
+              <p>{{ country.region }}</p>
+            </h3>
 
-						<h3>
-							Sub Region:
-							<p>{{ country.region }}</p>
-						</h3>
+            <h3>
+              Sub Region:
+              <p>{{ country.region }}</p>
+            </h3>
 
-						<h3>
-							Capital:
-							<p v-for="lan in country.capital">{{ lan }}, &nbsp;</p>
-						</h3>
-					</div>
-				</template>
+            <h3>
+              Capital:
+              <p v-for="lan in country.capital">{{ lan }}, &nbsp;</p>
+            </h3>
+          </div>
+        </template>
 
-				<template v-for="item in countryDetails" :key="item.name.common">
-					<div class="special-details">
-						<h3>
-							Top level Domain:
-							<p>{{ item.tld[0] }}</p>
-						</h3>
+        <template v-for="item in countryDetails" :key="item.name.common">
+          <div class="special-details">
+            <h3>
+              Top level Domain:
+              <p>{{ item.tld[0] }}</p>
+            </h3>
 
-						<h3>
-							Currencies:
-							<p v-for="currency in item.currencies">{{ currency.name }}</p>
-						</h3>
+            <h3>
+              Currencies:
+              <p v-for="currency in item.currencies">{{ currency.name }}</p>
+            </h3>
 
-						<h3>
-							Langauges:
-							<p v-for="lan in item.languages">{{ lan }}, &nbsp;</p>
-						</h3>
-					</div>
+            <h3>
+              Langauges:
+              <p v-for="lan in item.languages">{{ lan }}, &nbsp;</p>
+            </h3>
+          </div>
 
-					<div class="border">
-						<h3 v-show="item.borders">Border countries</h3>
-						<span v-for="neighbour in item.borders"
-							>{{ neighbour }}, &nbsp;</span
-						>
-					</div>
-				</template>
-			</div>
-		</div>
-	</div>
+          <div class="border">
+            <h3 v-show="item.borders">Border countries</h3>
+            <span v-for="neighbour in item.borders"
+              >{{ neighbour }}, &nbsp;</span
+            >
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="sass" scoped>
+.details-page
+	padding: 2rem
+	height: 100%
+	a
+		text-decoration: none
+		button
+			padding: 0.5rem 1rem
+			display: flex
+			box-shadow: var(--shadow)
+			border: none
+			background-color: var(--elements)
+			justify-content: center
+			gap: 0.5rem
+			margin-bottom: 1rem
+			p
+				margin:0
+
+
+	img
+		border-radius: var(--radius)
+		margin-block: 2rem
+	h2
+		margin-bottom: 2rem
+	p
+			display: inline-block
+			margin-bottom: 1rem
+			font-weight: normal
+
+@media (min-width:650px)
 	.details-page
-		padding: 2rem
-		height: 100%
-		a
-			text-decoration: none
-			color:inherit
-			button
-				padding: 0.5rem 1rem
-				display: flex
-				box-shadow: var(--shadow)
-				border: none
-				background-color: var(--elements)
-				justify-content: center
-				gap: 0.5rem
-				margin-bottom: 1rem
-				p
-					margin:0
+		button
+			box-shadow: initial
+		div
+			height: 100%
+			padding: 1em
 
 
-				.arrow
-					stroke:green
-
-		img
-			border-radius: var(--radius)
-			margin-block: 2rem
-		h2
-			margin-bottom: 2rem
-		p
-				display: inline-block
-				margin-bottom: 1rem
-				font-weight: normal
-
-	@media (min-width:650px)
-		.details-page
-			button
-				box-shadow: initial
-			div
-				height: 100%
-				padding: 1em
+			.details-container
+				display: grid
+				grid-template-columns: repeat(4,1fr)
+				grid-template-rows: repeat(4,1fr)
+				// align-items: center
+				gap:1rem
 
 
-				.details-container
-					display: grid
-					grid-template-columns: repeat(4,1fr)
-					grid-template-rows: repeat(4,1fr)
-					// align-items: center
-					gap:1rem
-
-
-					img
-						grid-column:  1/3
-						grid-row: 1/4
-						// justify-self: center
-						width: 100%
-					.basic-details
-						grid-column:  3/4
-						grid-row: 1/span2
-						grid-template-columns: subgrid
-						h2
-							grid-row: 1/2
-					.special-details
-						padding-top:4rem
-						grid-column: 4/5
-						grid-row:  1/3
-						justify-self: center
-						align-self: start
-					.border
-						grid-column: 3/span2
-						grid-row:  3/4
+				img
+					grid-column:  1/3
+					grid-row: 1/4
+					// justify-self: center
+					width: 100%
+				.basic-details
+					grid-column:  3/4
+					grid-row: 1/span2
+					grid-template-columns: subgrid
+					h2
+						grid-row: 1/2
+				.special-details
+					padding-top:4rem
+					grid-column: 4/5
+					grid-row:  1/3
+					justify-self: center
+					align-self: start
+				.border
+					grid-column: 3/span2
+					grid-row:  3/4
 </style>
